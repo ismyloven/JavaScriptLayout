75cf434fa875920595f8890e9b54395f75b1652a
Nail Ismailov <dd030789ine@gmail.com>
2020-11-16 18:41:44 +0200

    add_the_date

--- src/HW_js/HW_17_timer.js
+++ src/HW_js/HW_17_timer.js
@@ -2,6 +2,9 @@
 let hours = document.querySelector('.hours') ;
 let minutes = document.querySelector('.minutes') ;
 let seconds = document.querySelector('.seconds') ;
+let timer = document.querySelector('.thedate') ;
+
+
 
 function sayTime() {
 	let date = new Date;
@@ -55,3 +58,27 @@ function sayTime() {
 
 }
 setInterval(sayTime, start = true, 1000);
+
+
+
+let thedate = new Date();
+
+function formatDate(date) {
+
+	var day = date.getDate();
+	if (day < 10) day = '0' + day;
+	timer.children[0].innerHTML = day;
+
+	var month = date.getMonth() + 1;
+	if (month < 10) month = '0' + month;
+	timer.children[1].innerHTML = month;
+
+	var year = date.getFullYear() % 100;
+	if (year < 10) year = '0' + year;
+	timer.children[2].innerHTML = year;
+
+
+}
+
+
+formatDate(thedate);
