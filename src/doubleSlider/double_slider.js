908be484b647ab360215875f2fa9a33cfe324c36
Nail Ismailov <dd030789ine@gmail.com>
2020-12-10 16:13:34 +0200

    HW20: two slider with one Class

--- src/doubleSlider/double_slider.js
+++ src/doubleSlider/double_slider.js
@@ -1,3 +1,110 @@
+
+
+
+const slider2 = document.querySelector(".slider2");
+const btnNext = document.getElementById("next");
+const btnPrev = document.getElementById("prev");
+let viewSlide = 0;
+let startView;
+
+const slider3 = document.querySelector(".slider3");
+const viewSliders2 = document.querySelectorAll(".viewSlide2");
+let viewSlide2 = 0;
+
+var FirstSlider  = class {
+	constructor() {
+	}
+
+	handlerPrev () {
+	if (viewSlide < 3) {
+			viewSlide++;
+		} else {
+			viewSlide = 0;
+		}
+		slider2.style.left = `${-viewSlide * 450 + "px"}`;
+	}
+	handlerNext () {
+
+		if (viewSlide > 0) {
+			viewSlide--;
+		} else {
+			viewSlide = 3;
+		}
+		slider2.style.left = `${-viewSlide * 450 + "px"}`;
+	}
+	sliderWork() {
+		setInterval(firstSlide.handlerPrev,3000);
+	}
+	sliderStop() {
+		clearTimeout(startView);
+	}
+
+}
+
+let firstSlide = new FirstSlider();
+
+firstSlide.handlerPrev();
+firstSlide.handlerNext();
+btnNext.addEventListener("click", firstSlide.handlerPrev);
+btnPrev.addEventListener("click", firstSlide.handlerNext);
+
+
+slider2.addEventListener("mouseout", firstSlide.sliderWork);
+slider2.addEventListener("mouseover", firstSlide.sliderStop);
+
+
+startView = setInterval(firstSlide.handlerPrev,3000);
+
+
+class SecondSlider extends FirstSlider {
+	constructor(){
+		super();
+	}
+
+	handlerPrev () {
+		viewSliders2[viewSlide2].style.backgroundColor = "yellow";
+
+		if (viewSlide2 < 3) {
+
+			viewSlide2++;
+		} else {
+
+			viewSlide2 = 0;
+		}
+
+		viewSliders2[viewSlide2].style.backgroundColor = "pink";
+		slider3.style.left = `${-viewSlide2 * 450 + "px"}`;
+
+	}
+	handlerNext () {
+		viewSliders2[viewSlide2].style.backgroundColor = "yellow";
+		if (viewSlide2 > 0) {
+
+			viewSlide2--;
+		} else {
+
+			viewSlide2 =3;
+		}
+		viewSliders2[viewSlide2].style.backgroundColor = "pink";
+		slider3.style.left = `${-viewSlide2 * 450 + "px"}`;
+	}
+
+}
+
+
+let secondSlide = new SecondSlider();
+
+setInterval(secondSlide.handlerPrev,2000);
+
+secondSlide.handlerPrev();
+secondSlide.handlerNext();
+
+
+
+
+
+
+
 //
 // const slider2 = document.querySelector(".slider2");
 // const btnNext = document.getElementById("next");
@@ -53,94 +160,6 @@
 //
 // setInterval(handlerPrev,2000);
 //
-
-
-const slider2 = document.querySelector(".slider2");
-const btnNext = document.getElementById("next");
-const btnPrev = document.getElementById("prev");
-const viewSlide = 0;
-
-const slider3 = document.querySelector(".slider3");
-const viewSliders2 = document.querySelectorAll(".viewSlide2");
-const viewSlide2 = 0;
-
-var FirstSlider = class {
-	constructor() {
-	}
-
-	handlerPrev () {
-
-
-
-		if (viewSlide < 3) {
-			viewSlide++;
-		} else {
-			viewSlide = 0;
-		}
-		slider2.style.left = `${-viewSlide * 450 + "px"}`;
-	}
-	handlerNext () {
-
-		if (viewSlide > 0) {
-			viewSlide--;
-		} else {
-			viewSlide = 3;
-		}
-		slider2.style.left = `${-viewSlide * 450 + "px"}`;
-	}
-
-}
-
-
-
-class SecondSlider extends FirstSlider {
-	constructor(){
-		super();
-	}
-
-	handlerPrev () {
-		viewSliders2[viewSlide2].style.backgroundColor = "yellow";
-
-		if (viewSlide2 < 3) {
-
-			viewSlide2++;
-		} else {
-
-			viewSlide2 = 0;
-		}
-
-		viewSliders2[viewSlide2].style.backgroundColor = "pink";
-		slider3.style.left = `${-viewSlide2 * 450 + "px"}`;
-
-	}
-	handlerNext () {
-		viewSliders2[viewSlide2].style.backgroundColor = "yellow";
-		if (viewSlide2 > 0) {
-
-			viewSlide2--;
-		} else {
-
-			viewSlide2 =3;
-		}
-		viewSliders2[viewSlide2].style.backgroundColor = "pink";
-		slider3.style.left = `${-viewSlide2 * 450 + "px"}`;
-	}
-
-}
-
-let firstSlide = new FirstSlider;
-let secondSlide = new SecondSlider();
-
-btnNext.addEventListener("click", firstSlide.handlerPrev());
-btnPrev.addEventListener("click", firstSlide.handlerNext());
-
-setInterval(firstSlide.handlerPrev(),2000);
-setInterval(secondSlide.handlerPrev(),4000);
-
-firstSlide.handlerPrev();
-firstSlide.handlerNext();
-secondSlide.handlerPrev();
-secondSlide.handlerNext();
 
 
 
