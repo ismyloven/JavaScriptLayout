f035b5f6abea21474018b5ae1aaac24ec6fca2aa
Nail Ismailov <dd030789ine@gmail.com>
2020-12-13 23:51:46 +0200

    HW: infinity slider

--- src/doubleSlider/double_slider.js
+++ src/doubleSlider/double_slider.js
@@ -1,248 +1,107 @@
-const slider2 = document.querySelector(".slider2");
-const btnNext = document.getElementById("next");
-const btnPrev = document.getElementById("prev");
-let viewSlide = 0;
+
+
 let startView;
+let dots = document.getElementsByClassName("viewSlide2");
 
+const MainSlider = class  {
 
-const slider3 = document.querySelector(".slider3");
-const viewSliders2 = document.querySelectorAll(".viewSlide2");
-let viewSlide2 = 0;
-
-
-var MainSlideShow = class {
-	constructor() {
-	}
-
-	handlerPrev() {
-		if (viewSlide < 3) {
-			viewSlide++;
-
-		} else {
-			viewSlide = 0;
-
-		}
-		slider2.style.left = `${-viewSlide * 450 + "px"}`;
-
+	constructor(element) {
+		this.element = element;
+		this.scoring = 0;
 	}
 
 	handlerNext() {
 
-		if (viewSlide > 0) {
-			viewSlide--;
+		(this.scoring < 3) ? this.scoring++ : this.scoring = 0;
 
-		} else {
-			viewSlide = 3;
+		document.querySelector(this.element).style.left = `${-this.scoring * 450 + "px"}`;
+	};
 
-		}
-		slider2.style.left = `${-viewSlide * 450 + "px"}`;
+	handlerPrev() {
 
+		(this.scoring > 0) ? this.scoring-- : this.scoring = 3;
 
-	}
+		document.querySelector(this.element).style.left = `${-this.scoring * 450 + "px"}`;
+	};
+
 }
 
-class FirstSliderCat extends MainSlideShow {
-	constructor() {
-		super();
+class FirstSlider extends MainSlider {
+
+	constructor(element, nextBtn, prevBtn) {
+		super(element);
+		this.nextBtn = nextBtn;
+		this.prevBtn = prevBtn;
 	}
 
-	sliderWork() {
-		startView = setInterval(firstSlide.handlerPrev, 2000);
+	buttonNext(nextBtn, element, scoring) {
+		document.querySelector(this.nextBtn).addEventListener("click", () => this.handlerNext())
+	};
+
+
+	buttonPrev(prevBtn, element, scoring) {
+		document.querySelector(this.prevBtn).addEventListener("click", () => this.handlerPrev())
+	};
+
+	sliderWork(element) {
+		startView = setInterval(() => firstSlide.handlerNext(), 2000);
 	}
 
-	sliderStop() {
+	sliderStop(element) {
 		clearTimeout(startView);
 	}
 
 }
 
-let firstSlide = new FirstSliderCat();
-startView = setInterval(firstSlide.handlerPrev, 2000);
 
-btnNext.addEventListener("click", firstSlide.handlerPrev);
-btnPrev.addEventListener("click", firstSlide.handlerNext);
-slider2.addEventListener("mouseout", firstSlide.sliderWork);
-slider2.addEventListener("mouseover", firstSlide.sliderStop);
+class SecondSlider extends MainSlider {
 
-
-class SecondSliderCat extends MainSlideShow {
-	constructor() {
-		super();
-	}
-
-	handlerPrev2() {
-		viewSliders2[viewSlide2].style.backgroundColor = "yellow";
-
-		if (viewSlide2 < 3) {
-
-			viewSlide2++;
-		} else {
-
-			viewSlide2 = 0;
-		}
-
-		viewSliders2[viewSlide2].style.backgroundColor = "pink";
-		slider3.style.left = `${-viewSlide2 * 450 + "px"}`;
+	constructor(element) {
+		super(element);
 
 	}
-
-	handlerNext2() {
-		viewSliders2[viewSlide2].style.backgroundColor = "yellow";
-		if (viewSlide2 > 0) {
-
-			viewSlide2--;
-		} else {
-
-			viewSlide2 = 3;
-		}
-		viewSliders2[viewSlide2].style.backgroundColor = "pink";
-		slider3.style.left = `${-viewSlide2 * 450 + "px"}`;
-	}
-
+	// showDots(element){
+	// 	for (var i = 0; i < dots.length; i++) {
+	// 		console.log(dots[i]);
+	// 		dots[i] =  firstSlide.buttonNext();
+	// 		console.log(firstSlide.buttonNext());
+ 	// }
+	// }
 }
 
 
-let secondSlide = new SecondSliderCat();
+let firstSlide = new FirstSlider(".slider2", ".next", ".prev");
+let secondSlide = new SecondSlider(".slider3");
+document.querySelector(".slider2").addEventListener("mouseout", firstSlide.sliderWork);
+document.querySelector(".slider2").addEventListener("mouseover", firstSlide.sliderStop);
+firstSlide.buttonNext();
+firstSlide.buttonPrev();
 
-setInterval(secondSlide.handlerPrev2, 2000);
-
-secondSlide.handlerPrev2();
-secondSlide.handlerNext2();
 
 
+startView = setInterval(() => firstSlide.handlerNext(), 2000);
+setInterval(() => secondSlide.handlerNext(), 2000);
+
+
+// dots.addEventListener("click", secondSlide.showDots());
+
+
+
+
+
+// var slideIndex = 1;
+// function showSlides(n) {
+// 	var i;
+// 	var slides = document.getElementsByClassName("test");
+//     var dots = document.getElementsByClassName("viewSlide2");
 //
-// class SecondSliderCat extends MainSlideShow {
-// 	constructor(){
-// 		super();
+//
+// 	for (i = 0; i < dots.length; i++) {
+// 		dots[i].className = dots[i].className.replace("fon", "");
 // 	}
-//
-// 	handlerPrev () {
-// 		viewSliders2[viewSlide2].style.backgroundColor = "yellow";
-//
-// 		if (viewSlide2 < 3) {
-//
-// 			viewSlide2++;
-// 		} else {
-//
-// 			viewSlide2 = 0;
-// 		}
-//
-// 		viewSliders2[viewSlide2].style.backgroundColor = "pink";
-// 		slider2.style.left = `${-viewSlide2 * 450 + "px"}`;
-//
-// 	}
-// 	handlerNext () {
-// 		viewSliders2[viewSlide2].style.backgroundColor = "yellow";
-// 		if (viewSlide2 > 0) {
-//
-// 			viewSlide2--;
-// 		} else {
-//
-// 			viewSlide2 =3;
-// 		}
-// 		viewSliders2[viewSlide2].style.backgroundColor = "pink";
-// 		slider2.style.left = `${-viewSlide2 * 450 + "px"}`;
-// 	}
-//
+// 	slides[slideIndex - 1].style.display = "block";
+// 	dots[slideIndex - 1].className += " fon";
 // }
-//
-//
-// let secondSlide = new SecondSliderCat();
-//
-// setInterval(secondSlide.handlerPrev,2000);
-//
-// secondSlide.handlerPrev();
-// secondSlide.handlerNext();
-
-
-// var FirstSlider  = class {
-// constructor() {
-// }
-//
-// handlerPrev () {
-// if (viewSlide < 3) {
-// 		viewSlide++;
-// 	} else {
-// 		viewSlide = 0;
-// 	}
-// 	slider2.style.left = `${-viewSlide * 450 + "px"}`;
-// }
-// handlerNext () {
-//
-// 	if (viewSlide > 0) {
-// 		viewSlide--;
-// 	} else {
-// 		viewSlide = 3;
-// 	}
-// 	slider2.style.left = `${-viewSlide * 450 + "px"}`;
-// }
-// 	sliderWork() {
-// 		setInterval(firstSlide.handlerPrev,3000);
-// 	}
-// 	sliderStop() {
-// 		clearTimeout(startView);
-// 	}
-//
-// }
-//
-// let firstSlide = new MainSlider();
-//
-// firstSlide.handlerPrev();
-// firstSlide.handlerNext();
-// btnNext.addEventListener("click", firstSlide.handlerPrev);
-// btnPrev.addEventListener("click", firstSlide.handlerNext);
-//
-//
-// slider2.addEventListener("mouseout", firstSlide.sliderWork);
-// slider2.addEventListener("mouseover", firstSlide.sliderStop);
-//
-//
-// startView = setInterval(firstSlide.handlerPrev,3000);
-//
-//
-// class SecondSlider extends FirstSlider {
-// 	constructor(){
-// 		super();
-// 	}
-//
-// 	handlerPrev () {
-// 		viewSliders2[viewSlide2].style.backgroundColor = "yellow";
-//
-// 		if (viewSlide2 < 3) {
-//
-// 			viewSlide2++;
-// 		} else {
-//
-// 			viewSlide2 = 0;
-// 		}
-//
-// 		viewSliders2[viewSlide2].style.backgroundColor = "pink";
-// 		slider3.style.left = `${-viewSlide2 * 450 + "px"}`;
-//
-// 	}
-// 	handlerNext () {
-// 		viewSliders2[viewSlide2].style.backgroundColor = "yellow";
-// 		if (viewSlide2 > 0) {
-//
-// 			viewSlide2--;
-// 		} else {
-//
-// 			viewSlide2 =3;
-// 		}
-// 		viewSliders2[viewSlide2].style.backgroundColor = "pink";
-// 		slider3.style.left = `${-viewSlide2 * 450 + "px"}`;
-// 	}
-//
-// }
-//
-//
-// let secondSlide = new SecondSlider();
-//
-// setInterval(secondSlide.handlerPrev,2000);
-//
-// secondSlide.handlerPrev();
-// secondSlide.handlerNext();
-
-
+// showSlides(4);
 
 
--- src/slidershow/sliderjs.js
+++ src/slidershow/sliderjs.js
@@ -1,16 +1,7 @@
-const slider = document.querySelector(".newslider");
 let score = 0;
-
 function nextSlide() {
-
-	if (score < 3) {
-		score++;
-	} else {
-		score = 0;
-	}
-
-	slider.style.left = `${-score * 700 + "px"}`;
+	 (score < 3) ?score++ :score = 0;
+	document.querySelector(".newslider").style.left = `${-score * 700 + "px"}`;
 }
-
 setInterval(nextSlide, 2000);
 
